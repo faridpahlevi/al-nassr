7855cfe5ca1d8a907ec660b5168a682cf2ff378f
faridpahlevi <faridpahlevi@student.uir.ac.id>
2023-04-10 13:50:10 +0700

    deployment

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const { google } = require("googleapis");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
+const google_app_credentials = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
+
+app.use(express.static("./client/build"));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 app.get("/M01AB", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
-    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    credentials: google_app_credentials,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
@@ -23,13 +41,12 @@ app.get("/M01AB", async (req, res) => {
 
 app.get("/M01AE", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
-    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    credentials: google_app_credentials,
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
   const client = await auth.getClient();
   const googleSheets = google.sheets({ version: "v4", auth: client });
-  const spreadsheetId = "1H2fNSS_iLxgyos5ksR1OcNjV6q5bZtYf8GH5hvPHB1E";
 
   const getRows = await googleSheets.spreadsheets.values.get({
     auth,
@@ -39,4 +56,4 @@ app.get("/M01AE", async (req, res) => {
   res.json(getRows.data.values);
 });
 
-app.listen(1337, (req, res) => console.log("running on 1337"));
+app.listen(1337, (req, res) => console.log("running on 5000"));
